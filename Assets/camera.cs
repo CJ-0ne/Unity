fea1ed88d7d72f343a1c23f341a2db26533207c2
Hyeonseo071 <skybluehs0718@gmail.com>
2024-07-10 01:17:46 +0900

    first

--- Assets/camera.cs
+++ Assets/camera.cs
@@ -5,12 +5,23 @@ using UnityEngine;
 
 public class MainCamera : MonoBehaviour
 {
-    public Transform playTransform;
-    public Vector3 offset;
+    public GameObject Target;
 
+    public float offsetX = -55f;            // 카메라의 x좌표
+    public float offsetY = 6.8f;           // 카메라의 y좌표
+    public float offsetZ = -25f;          // 카메라의 z좌표
+
+    public float CameraSpeed = 10.0f;
+    Vector3 TargetPos;
 
     void LateUpdate()
     {
-        transform.position = playTransform.position + offset;
+        TargetPos = new Vector3(
+            Target.transform.position.x + offsetX,
+            Target.transform.position.y + offsetY,
+            Target.transform.position.z + offsetZ
+            );
+        //카메라 움직임 smooth
+        transform.position = Vector3.Lerp(transform.position, TargetPos, Time.deltaTime * CameraSpeed);
     }
 }
